934128863c1f097ec386d2ce049665a275684ba2
Maxibanex <114372559+Maxibanex@users.noreply.github.com>
2023-11-13 03:27:08 -0400

    commit con archivos

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+do_token = "dop_v1_8dde1c18a5584fa55959494c3ec2c410fabcecc5827d072c3c7b35c0f2ffb9e1"
+pvt_key  = "C:\\Users\\STW\\Documents\\DigitalKeyPrivate"
---
+++ www-1.tf
@@ -0,0 +1,38 @@
+resource "digitalocean_droplet" "www-1" {
+  image = "ubuntu-23-10-x64"
+  name = "www-1"
+  region = "nyc3"
+  size = "s-1vcpu-1gb"
+  private_networking = true
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+            
+      # Install Docker
+      "sudo apt-get update",
+      "sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      "echo 'deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update",
+      "sudo apt-get -y install docker-ce docker-ce-cli containerd.io",
+
+      # Install Docker Compose
+      "sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+
+      # Install Git
+      "sudo apt-get -y install git"
+
+    ]
+  }
+}
