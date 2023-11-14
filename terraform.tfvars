33521cab830d894926b5b2bd9d9f92fa2475378a
Maxibanex <114372559+Maxibanex@users.noreply.github.com>
2023-11-13 23:13:15 -0400

    clave privada y main.tf

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,2 @@
 do_token = "dop_v1_ab2eecdd16ee3555554b76b3b3f5d16f9af2dd737889dee50511adb2105ba788"
-pvt_key  = ".\ClavePrivada\DigitalKeyPrivate"
+pvt_key  = "ClavePrivada\\DigitalKeyPrivate"
--- www-1.tf
+++ www-1.tf
@@ -18,13 +18,7 @@ resource "digitalocean_droplet" "www-1" {
     inline = [
       "export PATH=$PATH:/usr/bin",
             
-      # Install Docker
-      "sudo apt-get update",
-      "sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-      "echo 'deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-      "sudo apt-get update",
-      "sudo apt-get -y install docker-ce docker-ce-cli containerd.io",
+      
 
       # Install Docker Compose
       "sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
