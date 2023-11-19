a1caddc5ac22099e8d2e820f2c43d333549c6ad9
Maxibanex <114372559+Maxibanex@users.noreply.github.com>
2023-11-19 19:33:04 -0400

    cambio a main

--- www-1.tf
+++ www-1.tf
@@ -23,8 +23,8 @@ resource "digitalocean_droplet" "www-1" {
       "sudo apt-get -y install docker.io",
 
       # Instalar Docker Compose
-      curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-      chmod +x /usr/local/bin/docker-compose,  
+      "curl -L 'https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose",
+      "chmod +x /usr/local/bin/docker-compose",  
 
       # Install Docker
       #"sudo apt-get update",
